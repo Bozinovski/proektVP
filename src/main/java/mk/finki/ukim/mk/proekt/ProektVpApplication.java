ae8c8ca521975e0d4fb96c74d90814e655803c4c
Bozinovski <aleksandar.bozinovski@live.com>
2020-04-13 03:15:27 +0200

    Initital commit

---
+++ src/main/java/mk/finki/ukim/mk/proekt/ProektVpApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.mk.proekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProektVpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProektVpApplication.class, args);
+    }
+
+}
